f01117dcbb5786df3f0495654fa8b5ca2221ce19
reudy14 <44473910+reudy14@users.noreply.github.com>
2023-06-15 13:20:52 +0200

    Update reader.py

--- reader.py
+++ reader.py
@@ -2,7 +2,7 @@ import os
 import random
 from termcolor import colored
 
-file = open("C:/Users/jacob/OneDrive/Plocha/psiaQuestions.txt", encoding="utf-8")
+file = open("./questions.txt", encoding="utf-8")
 lines = file.readlines()
 file.close()
 
@@ -12,9 +12,14 @@ while True:
     line = random.choice(lines)
     completedLines.append(line)
     lines.remove(line)
-    print(colored(f"Remaining: {len(lines)}", "blue"))
-    print(colored(line.split(": ")[0], "green"), end="\n\n")
-    print(colored(line.split(": ")[1]))
+    array = line.split(": ")
+    if len(array) == 2:
+        print(colored(f"Remaining: {len(lines)}", "blue"))
+        print(colored(array[0], "green"), end="\n\n")
+        print(array[1])
+    else:
+        print(colored(f"Remaining: {len(lines)}\n\n", "red"))
+        print(array[0])
     input("")
     if(len(lines) == 0):
         lines, completedLines = (completedLines, lines)
