c7a73e30bfb0fc47544f67548c47cfb1cc287f9c
Reudy <jacobdorfman14@gmail.com>
2025-05-30 21:18:24 +0200

    added hints

--- reader.py
+++ reader.py
@@ -1,6 +1,9 @@
+# Fortmat for questions is {CATEGORY}: {Question}
+
 import os
 import random
 from termcolor import colored
+from readchar import readchar
 
 file = open("./questions.txt", encoding="utf-8")
 lines = file.readlines()
@@ -12,14 +15,20 @@ while True:
     line = random.choice(lines)
     completedLines.append(line)
     lines.remove(line)
-    array = line.split(": ")
-    if len(array) == 2:
+    array = [a.strip() for a in line.split(":")]
+    if len(array) >= 2:
         print(colored(f"Remaining: {len(lines)}", "blue"))
-        print(colored(array[0], "green"), end="\n\n")
+        print(colored(array[0] + (" (HINT)" if len(array) >= 3 else ""), "green"), end="\n\n")
         print(array[1])
     else:
         print(colored(f"Remaining: {len(lines)}\n\n", "red"))
         print(array[0])
-    input("")
+    key = readchar()
+    if key == "q":    
+        os.system("cls")
+        exit(0)
+    if key == "s" and len(array) >= 3:
+        print(array[2])
+        readchar()
     if(len(lines) == 0):
         lines, completedLines = (completedLines, lines)
